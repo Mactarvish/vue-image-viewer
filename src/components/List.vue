8bf4df80cfcdd88c0dd123c35681a52150cd91a3
Mactarvish <451143003@qq.com>
2023-03-29 10:08:32 +0800

    [opt] 优化list布局

---
+++ src/components/List.vue
@@ -0,0 +1,91 @@
+<html>
+    <body>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        <div class="folder">
+            <h3>11111</h3>
+            <div class="img-table">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+                <img src="/data/mck/fengyongronglu/mmdetection/aa.jpg" width="200" alt="">
+            </div>
+        </div>
+        
+    </body>
+</html>
+
+<style>
+    body {
+        display: flex;
+        align-items: flex-start;
+        flex-direction: column;
+    }
+    .img-table {
+        <!-- display: flex; -->
+    }
+    .folder {
+        border: solid 2px cornflowerblue;
+        padding: 0.5rem;
+        margin: 1rem;
+    }
+    h3 {
+        margin: 4px 0;
+    }
+
+</style>
